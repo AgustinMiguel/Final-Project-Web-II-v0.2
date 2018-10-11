c9c1467f916a9253794cd04597883bf10c0aa02d
Agustin Miguel <agustinmiguel96@gmail.com>
2018-10-11 16:51:07 -0300

    Todo re peola

--- route.php
+++ route.php
@@ -6,18 +6,15 @@ function parseURL($url)
 {
   $urlExploded = explode('/', $url);
   $arrayReturn[ConfigApp::$ACTION] = $urlExploded[0];
-  #borrar/1/2/3/4
   $arrayReturn[ConfigApp::$PARAMS] = isset($urlExploded[1]) ? array_slice($urlExploded,1) : null;
   return $arrayReturn;
 }
 if(isset($_GET['action'])){
-   #$urlData[ACTION] = borrar
-   #$urlData[PARAMS] = [1,2,3,4]
     $urlData = parseURL($_GET['action']);
-    $action = $urlData[ConfigApp::$ACTION]; //home
+    $action = $urlData[ConfigApp::$ACTION];
     if(array_key_exists($action,ConfigApp::$ACTIONS)){
         $params = $urlData[ConfigApp::$PARAMS];
-        $action = explode('#',ConfigApp::$ACTIONS[$action]); //Array[0] -> TareasController [1] -> BorrarTarea
+        $action = explode('#',ConfigApp::$ACTIONS[$action]);
         $controller =  new $action[0]();
         $metodo = $action[1];
         if(isset($params) &&  $params != null){
